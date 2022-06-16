27f217a6243a68636c10093d0c74eaf2df554e03
Elijah Green <elijahgreen@gmail.com>
2022-06-15 18:29:55 -0700

    Change url for TOKEN_SERVER

--- src/shared.ts
+++ src/shared.ts
@@ -1,7 +1,5 @@
 export const API_KEY = "MWRiNTQ1NDctM2EyYi00NzcxLTlhNWYtMDdlNGViM2NkYWJj";
 // Secret is here on purpose
 // Otherwise would need to run a server.
-// export const API_SECRET = "YjUxYzBmODAtNjY1ZS00MjcxLThiYmYtNjVmNjNlODdhZDk4";
-// export const TOKEN_URL = "https://api.napster.com/oauth/access_token";
 export const TOKEN_SERVER =
-  "https://audiogata-oauth.audio-pwa.workers.dev/token";
+  "https://cloudflare-worker-token-service.audio-pwa.workers.dev/token";
